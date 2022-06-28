cf21d81eabe5504b97db8949edb670373a5dabba
andreaschs <90310505+sisfodkn@users.noreply.github.com>
2022-06-29 01:35:19 +0700

    Properties
    File properties untuk data

---
+++ app/Config/Properties.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Properties extends BaseConfig
+{
+    public $submit = "Submit";
+    public $reset = "Reset";
+    public $namaUnit = "Nama Unit";
+}
